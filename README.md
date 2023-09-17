5e9a18d73317d3239003acdc624f261b8078328f
Kristofer Berggren <kristofer.berggren@gmail.com>
2023-09-17 19:23:32 +0800

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# homebrew-nchat
-Homebrew tap for nchat
+Nchat Homebrew Tap
+==================
+
+This repository contains the [nchat](https://github.com/d99kris/nchat) formulae
+for Homebrew.
+
+
+Installation
+------------
+
+**Dependencies**
+Ensure XCode command line developer tools is installed:
+
+    xcode-select --install
+
+**Brew Install**
+
+    brew tap d99kris/nchat
+    brew install nchat
+
+
+Updating Formulae
+-----------------
+
+The repository includes a script which will check latest Github release version
+and update the formulae accordingly.
+
+    ./make.sh
+
+
+Testing Formulae Locally
+------------------------
+
+Test run the formulae locally with the following command.
+
+    brew install -vd ./Formula/nchat.rb
+
---
+++ make.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+# make.sh
+#
+# Copyright (c) 2023 Kristofer Berggren
+# All rights reserved.
+#
+# nchat is distributed under the MIT license.
+
+# This script updates a brew formula to point to latest available Github
+# release.
+
+REPOSITORY="https://github.com/d99kris/nchat"
+
+command -v curl > /dev/null
+if [[ "${?}" != "0" ]]; then
+  echo "curl is not installed."
+  exit 1
+fi
+
+RELEASE_URL=$(curl -Ls -o /dev/null -w %{url_effective} "${REPOSITORY}/releases/latest")
+if [[ "${RELEASE_URL}" == "" ]]; then
+  echo "cannot detetermine release url."
+  exit 1
+fi
+
+VERSION=$(echo "${RELEASE_URL}" | awk -F/ '{print $NF}')
+if [[ "${VERSION}" == "" ]]; then
+  echo "cannot detetermine version."
+  exit 1
+fi
+
+PACKAGE_URL="${REPOSITORY}/archive/refs/tags/${VERSION}.tar.gz"
+PACKAGE_SHA=$(curl -Ls "${PACKAGE_URL}" | sha256sum | cut -d' ' -f1)
+
+[[ "$(uname)" == "Linux" ]] && SEDCMD="sed" || SEDCMD="gsed"
+${SEDCMD} -i "s|^  url .*$|  url \"${PACKAGE_URL}\"|g" Formula/nchat.rb && \
+${SEDCMD} -i "s/^  sha256 .*$/  sha256 \"${PACKAGE_SHA}\"/g" Formula/nchat.rb
+if [[ "${?}" != "0" ]]; then
+  echo "formula update failed."
+  exit 1
+fi
+
+echo "success"
+exit 0
