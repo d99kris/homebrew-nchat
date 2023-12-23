51bff4662247c9fe7919fd98e6ea5a261629baf8
Kristofer Berggren <kristofer.berggren@gmail.com>
2023-12-23 18:00:36 +0800

    release v4.13

--- Formula/nchat.rb
+++ Formula/nchat.rb
@@ -1,8 +1,8 @@
 class Nchat < Formula
   desc "Terminal-based Telegram / WhatsApp client"
   homepage "https://github.com/d99kris/nchat"
-  url "https://github.com/d99kris/nchat/archive/refs/tags/v3.67.tar.gz"
-  sha256 "f7963bd510069d5f0f8fdf453ee0aea87facfe3c31a7552f12110867b9664e6e"
+  url "https://github.com/d99kris/nchat/archive/refs/tags/v4.13.tar.gz"
+  sha256 "9bacb57b550e75eba86f5c39f0570aa6600d785034930aeab82b2417dff1e3c4"
   license "MIT"
 
   depends_on "ccache"
