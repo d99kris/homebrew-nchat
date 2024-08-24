6bd2b7364ad0aa10090fcbcb3515843e0d9a3f4e
Kristofer Berggren <kristofer.berggren@gmail.com>
2024-08-24 10:08:39 +0800

    release v5.1.29

--- Formula/nchat.rb
+++ Formula/nchat.rb
@@ -1,8 +1,8 @@
 class Nchat < Formula
   desc "Terminal-based Telegram / WhatsApp client"
   homepage "https://github.com/d99kris/nchat"
-  url "https://github.com/d99kris/nchat/archive/refs/tags/v4.88.tar.gz"
-  sha256 "3a1c799f9658b5d90f05df0ecffd1b57921970f8bf7780a2946f19af6d9182b2"
+  url "https://github.com/d99kris/nchat/archive/refs/tags/v5.1.29.tar.gz"
+  sha256 "54dbae4743d3c51b723d2e92dbed2dcf6870a13d0b42da7fe680a96f342ebd3f"
   license "MIT"
 
   option "without-whatsapp"
